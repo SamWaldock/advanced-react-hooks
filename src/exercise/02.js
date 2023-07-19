96d42ee4cdb1b425a7bc43ea76e0b5e80069b138
Sam Waldock <swa@nexiosit.com>
2023-07-19 16:19:49 +0200

    useCallback (custom hooks): Make safeDispatch (extra credit 3)

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -10,6 +10,24 @@ import {
   PokemonErrorBoundary,
 } from '../pokemon'
 
+const useSafeDispatch = (dispatch) => {
+  const mountedRef = React.useRef(false);
+
+  React.useLayoutEffect(() => {
+    mountedRef.current = true;
+
+    return () => {
+      mountedRef.current = false;
+    }
+  })
+
+  return React.useCallback((...args) => {
+    if (mountedRef.current) {
+      dispatch(...args);
+    }
+  }, [dispatch])
+}
+
 const asyncReducer = (state, action) => {
   switch (action.type) {
     case 'pending': {
@@ -28,12 +46,14 @@ const asyncReducer = (state, action) => {
 }
 
 const useAsync = (initialState) => {
-  const [state, dispatch] = React.useReducer(asyncReducer, {
+  const [state, unsafeDispatch] = React.useReducer(asyncReducer, {
     status: 'idle',
     data: null,
     error: null,
     ...initialState,
-  })
+  });
+
+  const dispatch = useSafeDispatch(unsafeDispatch);
 
   const run = React.useCallback(promise => {
     dispatch({type: 'pending'})
@@ -45,7 +65,7 @@ const useAsync = (initialState) => {
         dispatch({type: 'rejected', error})
       },
     )
-  }, [])
+  }, [dispatch])
 
   return {...state, run};
 }
